dbc1e1ee5219b52392dedd08dfe1654f85f5b413
AcheZeta <ache.mariana@gmail.com>
2019-06-19 12:13:19 -0500

    First commit

---
+++ src/views/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="Login">
+    <input type="text" placeholder="Usuario" name="name" id="username"> <br>
+    <input type="password" placeholder="Contraseña" name="password" id="password"> <br>
+    <button> Registrarme </button>
+  </div>
+</template>
+
+<style>
+#Login {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#username, #password {
+  padding: .5%;
+  margin: .5%;
+}
+
+</style>
